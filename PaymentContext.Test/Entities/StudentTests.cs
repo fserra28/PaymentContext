8a752194a59632a4047eb871f0d2ca64ac9f172c
Filipe Serra <fserra28@gmail.com>
2018-12-26 12:03:05 -0200

    Criação de estrutura do projeto

---
+++ PaymentContext.Test/Entities/StudentTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Test
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            //var subscription = new Subscription(null);
+            //var student = new Student("Filipe Serra", "123456789", "fserra28@gmail.com");
+            //student.AddSubscription(subscription);
+
+            
+        }
+    }
+}
